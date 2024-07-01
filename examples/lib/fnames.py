c2d716a404b7f46e7b3dcabb9c2d7b901c273a74
Youngmin Park <ympark1988@gmail.com>
2024-07-01 15:34:32 -0400

    removed forcing flag from fnames

--- examples/lib/fnames.py
+++ examples/lib/fnames.py
@@ -55,7 +55,7 @@ def load_fnames_response(obj,model_pars=''):
 
 def load_fnames_nm(system,obj,model_pars='',coupling_pars=''):
 
-    pars = (system.dir1,system.miter,obj.forcing)
+    pars = (system.dir1,system.miter)
     system.G['fname'] = '{}G_{}_f={}.d'.format(*pars)
     system.K['fname'] = '{}K_{}_f={}.d'.format(*pars)
     system.p['fname'] = '{}p_{}_f={}.d'.format(*pars)
@@ -63,12 +63,10 @@ def load_fnames_nm(system,obj,model_pars='',coupling_pars=''):
     
     val = '{}p_data_ord={}_NP={}_NH={}_piter={}_n={}_m={}_f={}.txt'
     system.p['fnames_data'] = [val.format(system.dir1,k,obj.NP,obj.NH,
-                                          obj.pfactor,obj._n[1],obj._m[1],
-                                          obj.forcing)
+                                          obj.pfactor,obj._n[1],obj._m[1])
                                for k in range(system.miter)]
     
     val = '{}h_data_ord={}_NP={}_NH={}_piter={}_n={}_m={}_f={}.txt'
     system.h['fnames_data'] = [val.format(system.dir1,k,obj.NP,obj.NH,
-                                          obj.pfactor,obj._n[1],obj._m[1],
-                                          obj.forcing)
+                                          obj.pfactor,obj._n[1],obj._m[1])
                                for k in range(system.miter)]
