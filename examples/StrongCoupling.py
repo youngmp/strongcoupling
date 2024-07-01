df094fc706abe1f44f2360289f71471f0b63c42d
Youngmin Park <ympark1988@gmail.com>
2024-07-01 15:12:43 -0400

    added remaining functions

--- examples/StrongCoupling.py
+++ examples/StrongCoupling.py
@@ -75,8 +75,7 @@ def module_exists(module_name):
 class StrongCoupling2(object):
 
     def __init__(self,system1,system2,
-                 NP=100,NH=100,
-                 trunc_deriv=3,
+                 NH=100,trunc_deriv=3,
                  
                  _n=(None,1),_m=(None,1),
                  
@@ -87,6 +86,8 @@ class StrongCoupling2(object):
                  save_fig=False,
                  
                  recompute_list=[]):
+
+        
         self.save_fig = save_fig
 
         self._expand_kws = {'basic':True,'deep':True,'power_base':False,
@@ -101,27 +102,19 @@ class StrongCoupling2(object):
 
         self.system1 = system1;self.system2 = system2
         self.recompute_list = recompute_list
-        self.lowdim = lowdim
 
         self.trunc_deriv = trunc_deriv
-        self._n = _n;self._m = _m
-        #self.Tx = self.system1.T/self._n[1]
-        #self.Ty = self.system2.T/self._m[1]
 
-        self.om = self._n[1]/self._m[1]
-        self.T = self._m[1]*2*np.pi
-
-        self.system1.pardict['om'+str(system1.idx)] = self._n[1]
-        self.system2.pardict['om'+str(system2.idx)] = self._m[1]
+        self.om = 1
 
         ths_str = ' '.join(['th'+str(i) for i in range(2)])
         self.ths = symbols(ths_str,real=True)
 
         # discretization for p_X,p_Y
-        self.NP = NP
-        self.x,self.dx = np.linspace(0,2*np.pi,NP,retstep=True,endpoint=False)
+        self.NP = self.NH
+        self.x,self.dx = np.linspace(0,2*np.pi,self.NP,retstep=True,endpoint=False)
 
-        self.an,self.dan = np.linspace(0,2*np.pi*self._n[1],NP*self._n[1],
+        self.an,self.dan = np.linspace(0,2*np.pi*self._n[1],self.NP*self._n[1],
                                        retstep=True,endpoint=False)
 
         # discretization for H_X,H_Y
@@ -182,6 +175,513 @@ class StrongCoupling2(object):
                 system1.rule_i[ki] = system1.i['imp_'+key][k](in1)
 
         self._init_noforce(system1,system2)
+
+
+    def _init_noforce(self,system1,system2):
+        self.t,self.dt = np.linspace(0,2*np.pi,100,retstep=True)
+
+        if self.pfactor is None:
+            pfactor1 = int((np.log(.05)/system1.kappa_val))
+            pfactor2 = int((np.log(.05)/system2.kappa_val))
+            
+            self.pfactor = max([pfactor1,pfactor2])
+        
+        # just keep these here.
+        system2.h['dat']={};system2.h['imp']={};system2.h['lam']={}
+        system2.h['sym']={};system2.p['dat']={};system2.p['imp']={}
+        system2.p['lam']={};system2.p['sym']={}
+        
+        
+        for j,key in enumerate(system2.var_names):
+            in1 = self.ths[system2.idx]
+            system2.rule_lc[system2.syms[j]] = system2.lc['imp_'+key](in1)
+        
+        for key in system2.var_names:
+            for k in range(system2.miter):
+                in1 = self.ths[system2.idx]
+                
+                kg = sym.Indexed('g_'+system2.model_name+'_'+key,k)
+                system2.rule_g[kg] = system2.g['imp_'+key][k](in1)
+
+                kz = sym.Indexed('z_'+system2.model_name+'_'+key,k)
+                system2.rule_z[kz] = system2.z['imp_'+key][k](in1)
+                
+                ki = sym.Indexed('i_'+system2.model_name+'_'+key,k)
+                system2.rule_i[ki] = system2.i['imp_'+key][k](in1)
+        
+        
+        fnames.load_fnames_nm(system1,self)
+        fnames.load_fnames_nm(system2,self)
+        slib.load_coupling_expansions(system1)
+        slib.load_coupling_expansions(system2)
+
+        # mix up terms. G and K
+        self.load_k_sym(system1,system2)
+        self.load_k_sym(system2,system1)
+
+        # load odes for p_i
+        self.load_p_sym(system1)
+        self.load_p_sym(system2)
+
+        for i in range(system1.miter):
+            self.load_p(system1,system2,i)
+            self.load_p(system2,system1,i)
+
+        self.load_h_sym(system1)
+        self.load_h_sym(system2)
+
+        for i in range(system1.miter):
+            self.load_h(system1,system2,i)
+            self.load_h(system2,system1,i)
+
+    def load_k_sym(self,system1,system2):
+        
+        """
+        G is the full coupling function expanded.
+        K is the same, but indexed by powers of eps.
+
+        """
+            
+        # check that files exist
+        val = not(os.path.isfile(system1.G['fname']))
+        val += not(os.path.isfile(system1.K['fname']))
+
+        files_dne = val
+
+        system1.G['sym'] = {}
+        system1.K['sym'] = {}
+        if 'k_'+system1.model_name in self.recompute_list or files_dne:
+            logging.info('* Computing G symbolic...')
+
+            rule_trunc = {}
+            for k in range(system1.miter,system1.miter+500):
+                rule_trunc.update({system1.eps**k:0})
+
+            self.generate_k_sym(system1,system2)
+
+            # now collect terms
+            # G contains full summation
+            # K contains terms of summation indexed by power
+            
+            for key in system1.var_names:
+
+                tmp = expand(system1.G['sym'][key],**self._expand_kws)
+                system1.G['sym'][key] = tmp.subs(rule_trunc)
+                
+                system1.K['sym'][key] = []
+                for k in range(system1.miter):
+                    e_term = tmp.coeff(system1.eps,k)
+                    system1.K['sym'][key].append(e_term)
+
+            dill.dump(system1.G['sym'],open(system1.G['fname'],'wb'),
+                      recurse=True)
+            dill.dump(system1.K['sym'],open(system1.K['fname'],'wb'),
+                      recurse=True)
+                
+        else:
+            logging.info('* Loading G symbolic...')
+            system1.G['sym'] = dill.load(open(system1.G['fname'],'rb'))
+            system1.K['sym'] = dill.load(open(system1.K['fname'],'rb'))
+
+        system1.G['vec'] = sym.zeros(system1.dim,1)
+
+        
+
+        for i,key in enumerate(system1.var_names):
+            system1.G['vec'][i] = [system1.G['sym'][key]]
+
+    def generate_k_sym(self,system1,system2):
+        """
+        generate terms involving the coupling term (see K in paper).
+        Equations (8), (9) in Park, Wilson 2021
+        """
+
+        psym = system1.pardict_sym # shorten for readability
+        fn = system1.coupling # coupling function
+
+        # all variables and d_variables
+        var_names_all = system1.var_names + system2.var_names
+        z = sym.Matrix(system1.syms + system2.syms)
+        dz = sym.Matrix(system1.dsyms + system2.dsyms)
+
+        # get full coupling term for given oscillator
+        system1.G['sym_fn'] = sym.flatten(fn(z,psym,option='sym',
+                                             idx=system1.idx))
+
+        if self.forcing:
+            assert(z[-1,0] == system2.syms[0])
+            z = z[:-1,:]
+            dz = dz[:-1,:]
+            
+        
+        # 0 and 1st derivative
+        c_temp = {}
+        for key_idx,key in enumerate(system1.var_names):
+            c_temp[key] = system1.G['sym_fn'][key_idx]
+            
+            d = lib.df(system1.G['sym_fn'][key_idx],z,1).dot(dz)
+            c_temp[key] += d
+            
+        # 2nd + derivative        
+        for key_idx,key in enumerate(system1.var_names):
+            for k in range(2,self.trunc_deriv+1):
+                kp = lib.kProd(k,dz)
+                da = lib.vec(lib.df(system1.G['sym_fn'][key_idx],z,k))
+                c_temp[key] += (1/math.factorial(k))*kp.dot(da)
+
+        # save 
+        for key in system1.var_names:
+            system1.G['sym'][key] = c_temp[key]
+
+                        
+        # replace dx with (g expansion in eps).
+        rule = {}
+
+        for key_idx,key in enumerate(system1.var_names):
+            rule.update({system1.dsyms[key_idx]:system1.g[key+'_eps']})
+        for key_idx,key in enumerate(system2.var_names):
+            rule.update({system2.dsyms[key_idx]:system2.g[key+'_eps']})
+
+        
+
+        for key in system1.var_names:
+            system1.G['sym'][key] = system1.G['sym'][key].subs(rule)
+        
+    def load_p_sym(self,system1):
+        """
+        generate/load the het. terms for psi ODEs.
+            
+        to be solved using integrating factor meothod.        
+        system*.p[k] is the forcing function for oscillator * of order k
+        """
+
+        file_dne = not(os.path.isfile(system1.p['fname']))
+
+        eps = system1.eps
+
+        if 'p_'+system1.model_name in self.recompute_list or file_dne:
+            logging.info('* Computing p symbolic...')
+            print('* Computing p symbolic...')
+            
+            rule_trunc = {}
+            for k in range(system1.miter,system1.miter+500):
+                rule_trunc.update({eps**k:0})
+
+            v1 = system1.i['vec']; v2 = system1.G['vec']
+            
+            tmp = eps*v1.dot(v2)
+            tmp = expand(tmp,**self._expand_kws)
+            
+            tmp = tmp.subs(rule_trunc)
+            tmp = collect(tmp,system1.eps)
+            tmp = collect(expand(tmp),system1.eps)
+
+            for k in range(system1.miter):
+                system1.p['sym'][k] = tmp.coeff(system1.eps,k)
+
+            dill.dump(system1.p['sym'],open(system1.p['fname'],'wb'),
+                      recurse=True)
+
+        else:
+            logging.info('* Loading p symbolic...')
+            print('* Loading p symbolic...')
+            system1.p['sym'] = dill.load(open(system1.p['fname'],'rb'))
+
+        
+
+    def load_p(self,system1,system2,k):
+
+        fname = system1.p['fnames_data'][k]
+        file_dne = not(os.path.isfile(fname))
+        
+        if 'p_data_'+system1.model_name in self.recompute_list\
+           or file_dne:
+            
+            
+            p_data = self.generate_p(system1,system2,k)
+
+
+            #if not(self.forcing):
+            #p_interp0 = interp2d([0,0],[2*np.pi*self._n[1],
+            #                            2*np.pi*self._m[1]],
+            #                     [self.dan,self.dan],
+            #                     p_data,k=5,p=[True,True])
+
+            # fix indexing (see check_isostable.ipynb)
+            #X,Y = np.meshgrid(self.an*self._n[1],
+            #                  self.an*self._m[1],
+            #                  indexing='ij')
+            #p_data = p_interp0(X-self.om*Y,Y)
+                
+            np.savetxt(system1.p['fnames_data'][k],p_data)
+
+        else:
+            
+            p_data = np.loadtxt(fname)
+
+        system1.p['dat'][k] = p_data
+
+        n=self._n[1];m=self._m[1]
+        x=self.x;dx=self.dx
+        
+        p_interp = interp2d([0,0],[x[-1]*n,x[-1]*m],[dx*n,dx*m],p_data,
+                            k=9,p=[True,True])
+
+        ta = self.ths[0];tb = self.ths[1]
+        name = 'p_'+system1.model_name+'_'+str(k)
+        p_imp = imp_fn(name,self.fast_interp_lam(p_interp))
+        lamtemp = lambdify(self.ths,p_imp(ta,tb))
+        
+
+        if k == 0:
+            imp = imp_fn('p_'+system1.model_name+'_0', lambda x: 0*x)
+            system1.p['imp'][k] = imp
+            system1.p['lam'][k] = 0
+            
+            
+        else:
+            system1.p['imp'][k] = p_imp
+            system1.p['lam'][k] = lamtemp#p_interp
+
+        if self.save_fig:
+            
+            fig,axs = plt.subplots()
+            axs.imshow(p_data)
+            pname = 'figs_temp/p_'
+            pname += system1.model_name+str(k)
+            pname += '_'+str(n)+str(m)
+            pname += '.png'
+            plt.savefig(pname)
+            plt.close()
+
+        # put these implemented functions into the expansion
+        system1.rule_p.update({sym.Indexed('p_'+system1.model_name,k):
+                               system1.p['imp'][k](ta,tb)})
+
+
+    def generate_p(self,system1,system2,k):
+        print('p order='+str(k))
+
+        n = self._n[1];m = self._m[1]
+        NP = self.NP
+        data = np.zeros([n*NP,m*NP])
+        
+        if k == 0:
+            #pA0 is 0 (no forcing function)
+            return data
+
+        kappa1 = system1.kappa_val
+
+        rule = {**system1.rule_p,**system2.rule_p,**system1.rule_par,
+                **system2.rule_par,**system1.rule_lc,**system2.rule_lc,
+                **system1.rule_i,**system2.rule_i,**system1.rule_g,
+                **system2.rule_g}
+        
+        if self.forcing:
+            imp0 = imp_fn('forcing',system2)
+            lam0 = lambdify(self.ths[1],imp0(self.ths[1]))
+            rule.update({system2.syms[0]:imp0(self.ths[1])})
+
+        ph_imp1 = system1.p['sym'][k].subs(rule)
+
+        if ph_imp1 == 0: # keep just in case
+            return np.zeros([NP,NP])
+        
+        lam1 = lambdify(self.ths,ph_imp1)        
+        
+        x=self.x;dx=self.dx;pfactor=self.pfactor
+        sm=np.arange(0,self.T*pfactor,dx)*m
+        
+        fac = self.om*(1-system1.idx) + system1.idx
+        exp1 = exp(fac*sm*system1.kappa_val)
+
+        g_in = np.fft.fft(exp1)
+        a_i = np.arange(NP,dtype=int)
+        
+        #for ll in range(len(an)):
+        for ll in range(len(x)):
+
+            f_in = np.fft.fft(lam1(x[ll]*n+self.om*sm,sm))
+            conv = np.fft.ifft(f_in*g_in)
+            data[(a_i+ll)%int(n*NP),a_i] = conv[-int(m*NP):].real
+            #data[ll,:] = conv[-self._m[1]*NP:].real
+
+        return data*self.dan
+    
+    
+    def load_h_sym(self,system):
+        """
+        also compute h lam
+        """
+
+        fname = system.h['fname']
+        file_dne = not(os.path.isfile(fname))
+
+        # symbolic h terms
+        system.h['sym'] = {}
+        
+        if 'h_'+system.model_name in self.recompute_list or file_dne:
+            logging.info('* Computing H symbolic...')
+            print('* Computing H symbolic...')
+            
+            # simplify expansion for speed
+            rule_trunc = {}
+            for k in range(system.miter,system.miter+200):
+                rule_trunc.update({system.eps**k:0})
+
+            logging.info('Oscillator '+system.model_name)
+            v1 = system.z['vec']; v2 = system.G['vec']
+                    
+            tmp = v1.dot(v2)
+            tmp = sym.expand(tmp,**self._expand_kws)
+            
+            tmp = tmp.subs(rule_trunc)
+            tmp = collect(tmp,system.eps)
+            tmp = collect(expand(tmp),system.eps)
+
+            for k in range(system.miter):
+                system.h['sym'][k] = tmp.coeff(system.eps,k)
+
+            dill.dump(system.h['sym'],open(fname,'wb'),recurse=True)
+                
+        else:
+            logging.info('* Loading H symbolic...')
+            print('* Loading H symbolic...')
+            system.h['sym'] = dill.load(open(fname,'rb'))
+            
+    def load_h(self,system1,system2,k):
+
+        fname = system1.h['fnames_data'][k]
+        file_dne = not(os.path.isfile(fname))
+
+        if 'h_data_'+system1.model_name in self.recompute_list\
+           or file_dne:
+            str1 = '* Computing H {}, order={}...'
+            logging.info(str1.format(system1.model_name,k))
+            print(str1.format(system1.model_name,k))
+
+            h_data = self.generate_h(system1,system2,k)
+            np.savetxt(system1.h['fnames_data'][k],h_data)
+
+        else:
+            str1 = '* Loading H {}, order={}...'
+            logging.info(str1.format(system1.model_name,k))
+            print(str1.format(system1.model_name,k))
+            
+            h_data = np.loadtxt(fname)
+
+        system1.h['dat'][k] = h_data
+        hodd = h_data[::-1] - h_data
+
+        n=self._n[1];m=self._m[1]
+        #system1.h['lam'][k] = interpb(self.be,h_data,2*np.pi)
+        #system1.h['lam'][k] = interp1d(0,self.x[-1]*n,self.dx*n,
+        #                               h_data,p=True,k=9)
+
+        system1.h['lam'][k] = interp1d(self.bn[0],self.bn[-1],self.dbn,
+                                       h_data,p=True,k=5)
+
+        if self.save_fig:    
+            fig,axs = plt.subplots(figsize=(6,2))
+
+            axs.plot(h_data)
+
+            ptitle = 'h '+system1.model_name
+            ptitle += 'o='+str(k)
+            ptitle += ', nm='+str(n)+str(m)
+            axs.set_title(ptitle)
+            
+            pname = 'figs_temp/h_'
+            pname += system1.model_name+str(k)
+            pname += '_'+str(n)+str(m)
+            pname += '.png'
+            plt.savefig(pname)            
+            plt.close()
+
+                
+    def generate_h(self,system1,system2,k):
+
+        rule = {**system1.rule_p,**system2.rule_p,**system1.rule_par,
+                **system2.rule_par,**system1.rule_lc,**system2.rule_lc,
+                **system1.rule_g,**system2.rule_g,**system1.rule_z,
+                **system2.rule_z,**system1.rule_i,**system2.rule_i}
+        
+        if self.forcing:
+            imp0 = imp_fn('forcing',system2)
+            rule.update({system2.syms[0]:imp0(self.ths[1])})
+            
+        h_lam = lambdify(self.ths,system1.h['sym'][k].subs(rule))
+
+        """
+        x=self.x;dx=self.dx
+        n=self._n[1];m=self._m[1]
+        
+        X,Y = np.meshgrid(x*n,x*m,indexing='ij')
+        h_integrand = h_lam(X,Y)
+        ft2 = np.fft.fft2(h_integrand)
+        # get only those in n:m indices
+        # n*i,-m*i
+        ft2_new = list([ft2[0,0]])
+        ft2_new += list(np.diag(np.flipud(ft2[1:,1:]))[::-1])
+        ft2_new = np.asarray(ft2_new)
+        ft2_new /= np.sqrt(self.NH)*2
+
+        out = np.fft.ifft(ft2_new).real/(2*np.pi*m)
+        print('h mean',np.mean(out))
+        return out
+        """
+        
+        bn=self.be;dbn=self.dbe
+        #bn=self.be;dbn=self.dbe
+        n=self._n[1];m=self._m[1]
+
+        # calculate mean
+        #X,Y = np.meshgrid(self.an,self.an*self._m[1],indexing='ij')
+        #system1._H0[k] = np.sum(h_lam(X,Y))*self.dan**2/(2*np.pi*self._m[1])**2
+
+        def integrand(x,y):
+            return h_lam(y+self.om*x,x)
+                
+        # calculate coupling function
+        h = np.zeros(self.NH)
+        for j in range(self.NH):
+            #val = np.sum(h_lam(bn[j] + self.om*bn,bn))*dbn
+            val = quad(integrand,bn[0],bn[-1],args=(bn[j],),limit=10000)[0]
+
+            h[j] = val
+
+        out = h/(2*np.pi*self._m[1])
+
+        return out
+        
+    def fast_interp_lam(self,fn):
+        """
+        interp2db object (from fast_interp)
+        """
+        return lambda t0=self.ths[0],t1=self.ths[1]: fn(t0,t1)
+
+    
+    def save_temp_figure(self,data,k,fn='plot',path_loc='figs_temp/'):
+        """
+        data should be (TN,dim)
+        """
+
+        if (not os.path.exists(path_loc)):
+            os.makedirs(path_loc)
+        
+        fig, axs = plt.subplots(nrows=self.dim,ncols=1)
+        
+        for j,ax in enumerate(axs):
+            key = self.var_names[j]
+            ax.plot(self.tLC,data[:,j],label=key)
+            ax.legend()
+            
+        logging.info(fn+str(k)+' ini'+str(data[0,:]))
+        logging.info(fn+str(k)+' fin'+str(data[-1,:]))
+        axs[0].set_title(fn+str(k))
+        plt.tight_layout()
+        plt.savefig(path_loc+fn+str(k)+'.png')
+        plt.close()
 
 
 # below is the legacy strongcoupling class
